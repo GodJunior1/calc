07ba3293560421c20a96f470245db8b3e362947d
GodJunior1 <i_sachkov@inbox.ru>
2023-04-19 20:14:30 +0300

    calc start

---
+++ Calc/Calc/Calc.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	float a = 0;
+	float b = 0;
+	char operation = 0;
+
+	cout << "Введите первое число: " << endl;
+	cin >> a;
+
+	cout << "Введите второе число: " << endl;
+	cin >> b;
+
+	cout << "Выберите действие которое хотите произвезти с числами +, -, *, /: ";
+	cin >> operation;
+
+	switch (operation)
+	{
+	case '+':
+		cout << a + b<< endl;
+		break;
+	case '-':
+		cout << a - b<< endl;
+		break;
+	case '*':
+		cout << a * b<< endl;
+		break;
+	case '/':
+		cout << a / b << endl;
+		break;
+	
+
+	}
+
+
+
+}
+
